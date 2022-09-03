1eb2eb1d2883f5101cc488eaf8b56c17e0c5a28b
lee <1280403599@qq.com>
2022-09-04 02:23:52 +0800

    change vant

--- src/main.js
+++ src/main.js
@@ -3,56 +3,12 @@ import router from './router'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import 'amfe-flexible'
-import {
-  Button,
-  Icon,
-  Tab,
-  Tabs,
-  TreeSelect,
-  Stepper,
-  ActionBar,
-  ActionBarIcon,
-  ActionBarButton,
-  Toast,
-  Checkbox,
-  CheckboxGroup,
-  SubmitBar,
-  ContactCard,
-  Card,
-  Dialog,
-  AddressList,
-  AddressEdit,
-  Form,
-  Field,
-  CellGroup
-} from 'vant'
+
 import 'vant/lib/index.css'
 import './common/css/base.less'
 
 const app = createApp(App)
 
-app
-  .use(Button)
-  .use(Icon)
-  .use(Tab)
-  .use(Tabs)
-  .use(TreeSelect)
-  .use(Stepper)
-  .use(ActionBar)
-  .use(ActionBarIcon)
-  .use(ActionBarButton)
-  .use(Toast)
-  .use(Checkbox)
-  .use(CheckboxGroup)
-  .use(SubmitBar)
-  .use(ContactCard)
-  .use(Card)
-  .use(Dialog)
-  .use(AddressList)
-  .use(AddressEdit)
-  .use(Form)
-  .use(Field)
-  .use(CellGroup)
 app.use(router)
 app.use(createPinia())
 
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import { VantResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +10,10 @@ export default defineConfig({
     port: 8000,
     hmr: true
   },
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [VantResolver()]
+    })
+  ]
 })
