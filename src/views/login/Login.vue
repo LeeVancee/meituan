344410c3f3a626df92884e7c8a97fffe4f713876
lee <1280403599@qq.com>
2022-09-03 17:42:05 +0800

    add login and register pages

---
+++ src/views/login/Login.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="login">
+    <Header title="登录" />
+    <div class="img">滴团</div>
+    <van-form @submit="onSubmit">
+      <van-field
+        v-model="username"
+        name="用户名"
+        label="用户名"
+        placeholder="用户名"
+        :rules="[{ required: true, message: '请填写用户名' }]"
+      />
+      <van-field
+        v-model="password"
+        type="password"
+        name="密码"
+        label="密码"
+        placeholder="密码"
+        :rules="[{ required: true, message: '请填写密码' }]"
+      />
+      <div style="margin: 16px">
+        <van-button
+          round
+          block
+          type="info"
+          native-type="submit"
+          color="#ffc400"
+        >
+          登录
+        </van-button>
+        <van-button round block type="info" color="#ffc400" class="register">
+          注册
+        </van-button>
+      </div>
+    </van-form>
+  </div>
+</template>
+
+<script>
+import { reactive, toRefs } from 'vue'
+import Header from '../../components/Header.vue'
+
+export default {
+  components: {
+    Header
+  },
+  setup() {
+    let data = reactive({
+      username: '',
+      password: ''
+    })
+    // 提交按钮
+    const onSubmit = () => {}
+
+    return {
+      ...toRefs(data),
+      onSubmit
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.login {
+  .img {
+    width: 200px;
+    height: 200px;
+    background-color: #ffc400;
+    font-size: 80px;
+    line-height: 200px;
+    text-align: center;
+    border-radius: 40px;
+    margin: 20px auto;
+  }
+  .register {
+    margin-top: 20px;
+  }
+}
+</style>
